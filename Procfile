d8e40925db36b4cacc0c4f8a4787818f23a2f629
FaBLee <58670304+fablee1@users.noreply.github.com>
2021-07-20 17:25:46 +0100

    test

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: python3 webhook.py
+worker: python3 webhook.py
--- handlers/groups/reputation.py
+++ handlers/groups/reputation.py
@@ -34,7 +34,7 @@ async def increase_reputation(message: types.Message):
                     await message.answer(goal['message'].format(user=f"@{replied_to.username}", rep=replied_user['reputation']+1))
         else:
             m = await message.answer(f"🚨 @{m_from.username}, ты превысил лимит! 🚨")
-    await delete_message(m)
+    delete_message(m)
 
 @dp.message_handler(ChatTypeFilter(['group', 'supergroup']), content_types=types.ContentType.NEW_CHAT_MEMBERS)
 async def new_chat_member(message: types.Message):
