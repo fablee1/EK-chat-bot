d1f29deb49b19ba3006c0303c80007565c15fac8
FaBLee <58670304+fablee1@users.noreply.github.com>
2021-07-20 14:09:17 +0100

    update

--- webhook.py
+++ webhook.py
@@ -1,18 +1,18 @@
-# def set_hook():
-#     import asyncio
-#     from config import HEROKU_APP_NAME, WEBHOOK_URL, TOKEN
-#     from aiogram import Bot
-#     bot = Bot(token=TOKEN)
+def set_hook():
+    import asyncio
+    from config import HEROKU_APP_NAME, WEBHOOK_URL, TOKEN
+    from aiogram import Bot
+    bot = Bot(token=TOKEN)
 
-#     async def hook_set():
-#         if not HEROKU_APP_NAME:
-#             print('You have forgot to set HEROKU_APP_NAME')
-#             quit()
-#         await bot.set_webhook(WEBHOOK_URL)
-#         print(await bot.get_webhook_info())
+    async def hook_set():
+        if not HEROKU_APP_NAME:
+            print('You have forgot to set HEROKU_APP_NAME')
+            quit()
+        await bot.set_webhook(WEBHOOK_URL)
+        print(await bot.get_webhook_info())
 
-#     asyncio.run(hook_set())
-#     bot.close()
+    asyncio.run(hook_set())
+    bot.close()
 
 
 if __name__ == "__main__":
